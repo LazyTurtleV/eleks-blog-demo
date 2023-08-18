37869cdc598723c1e198c27c47c641a80a82301f
LazyTurtleV <savinvolodya001@gmail.com>
2023-08-18 22:36:02 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    })
+  ],
+  devServer: {
+    port: 3030,
+  },
+  ignoreWarnings: [
+    /only default export is available soon/,
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+};
