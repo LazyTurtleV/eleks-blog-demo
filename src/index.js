b7ae8030fd12d7cb7bb177ce3094434c773fbabd
LazyTurtleV <savinvolodya001@gmail.com>
2023-08-22 20:51:33 +0300

    basic navigation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    element: <div>s</div>,
+    path: 'stories',
+    element: <div>stories</div>
+  },
+  {
+    element: <div>s</div>,
+    path: '/stories/:storyId',
+    element: <div>A particular story</div>
+  },
+  {
+    path: 'login',
+    element: <div>Login</div>
+  },
+  {
+    path: 'signup',
+    element: <div>sign up</div>
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>My awesome app</div>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
