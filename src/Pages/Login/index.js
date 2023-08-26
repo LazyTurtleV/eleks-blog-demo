d39049b0407b56108b655b70db904aa46256cccd
LazyTurtleV <savinvolodya001@gmail.com>
2023-08-26 14:28:55 +0300

    add login form

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-import Header from '../Common/Header';
+import './styles.scss';
+
+import LoginHeader from './LoginHeader';
 
 export default function Login() {
-  return <Header />;
+  return (
+    <main className={'login-container'}>
+      <div className={'login-box'}>
+        <LoginHeader />
+        <form className={'login-form'}>
+          <h1>Log in</h1>
+          <label for={'email'}>Email</label>
+          <input name={'email'} type={'email'} />
+          <label for={'password'}>Password</label>
+          <input name={'password'} type={'password'} />
+        </form>
+      </div>
+    </main>
+  );
 }
