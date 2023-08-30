d8c08f708e249425632319eea0af7bc1e92741f3
LazyTurtleV <savinvolodya001@gmail.com>
2023-08-30 21:58:06 +0300

    basic stories page

---
+++ src/Pages/Stories/StoriesGrid.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import DataLayer from '../../Services/DataLayer';
+
+export default function StoriesGrid() {
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    DataLayer.getArticles().then((d) => {
+      if (Array.isArray(d)) {
+        setStories(d);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      {stories.map((story, i) => {
+        console.log(story.img);
+        //the order will never change so index as a key is ok, I guess
+        return <StoryItem key={i} img={story.img} author={story.author} />;
+      })}
+    </div>
+  );
+}
+
+function StoryItem({ img, header, author: { name } }) {
+  // console.log(img);
+  return (
+    <section>
+      <header>
+        <p>{name}</p>
+        <img src={img} alt={'story_image'} />
+        <h1>{header}</h1>
+      </header>
+    </section>
+  );
+}
--- src/Pages/Stories/index.js
+++ src/Pages/Stories/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+
 import Header from '../Common/Header';
+import StoriesGrid from './StoriesGrid';
+
+import './styles.scss';
 
 export default function Stories() {
   return (
     <>
       <Header />
+      <main>
+        <StoriesGrid />
+      </main>
     </>
   );
 }
