f2e41d83bac592f9df048e8d1dd1dbaaf7e11432
LazyTurtleV <savinvolodya001@gmail.com>
2023-09-02 17:32:36 +0300

    initial stories grid header

---
+++ src/Pages/Stories/StoriesGridHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function StoriesGridHeader() {
+  return (
+    <header className={'stories-grid-header'}>
+      <h1>Top stories</h1>
+      <button className={'add-story-btn'}>Add story</button>
+    </header>
+  );
+}
--- src/Pages/Stories/index.js
+++ src/Pages/Stories/index.js
@@ -4,12 +4,14 @@ import Header from '../Common/Header';
 import StoriesGrid from './StoriesGrid';
 
 import './styles.scss';
+import StoriesGridHeader from './StoriesGridHeader';
 
 export default function Stories() {
   return (
     <>
       <Header />
       <main>
+        <StoriesGridHeader />
         <StoriesGrid />
       </main>
     </>
