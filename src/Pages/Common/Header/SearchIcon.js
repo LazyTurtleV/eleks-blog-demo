d17f94129cea5b47e5d8cc1af235c6d5211fe7b5
LazyTurtleV <savinvolodya001@gmail.com>
2023-09-02 21:56:21 +0300

    hide search bar in mobile

---
+++ src/Pages/Common/Header/SearchIcon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import searchIcon from '../../../../assets/search.svg';
+
+export default function SearchIcon() {
+  return <img src={searchIcon} className={'search-icon'} />;
+}
--- src/Pages/Common/Header/index.js
+++ src/Pages/Common/Header/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import image from '../../../../assets/avatar.png';
 
 import './styles.scss';
 import Logo from './Logo';
-import { useNavigate } from 'react-router-dom';
 import SearchBar from './Searchbar';
+import useBreakpoints from '../useBreakpoints';
+import SearchIcon from './SearchIcon';
 
 export default function Header() {
   const navigate = useNavigate();
+  const { breakpoint } = useBreakpoints({
+    lowerBound: '500px',
+    upperBound: '900px',
+  });
   return (
     <header className="container">
       <Logo />
-      <SearchBar />
-      <img src={image} alt={'Avatar'} onClick={() => navigate('/profile')} />
+      {['large', 'medium'].includes(breakpoint) ? (
+        <SearchBar />
+      ) : (
+        <SearchIcon />
+      )}
+      <img
+        className={'user-avatar'}
+        src={image}
+        alt={'Avatar'}
+        onClick={() => navigate('/profile')}
+      />
     </header>
   );
 }
