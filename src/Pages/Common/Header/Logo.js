6cb73468c5a18632100e7e5e7d39d13544ef4574
LazyTurtleV <savinvolodya001@gmail.com>
2023-09-02 21:01:56 +0300

    make search bar more responsive

--- src/Pages/Common/Header/Logo.js
+++ src/Pages/Common/Header/Logo.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import SearchBar from './Searchbar';
 
 export default function () {
   return (
     <logo className={'logo'}>
       <h1>ELEKS</h1>
       <h1>BLOG</h1>
-      <SearchBar />
     </logo>
   );
 }
--- src/Pages/Common/Header/index.js
+++ src/Pages/Common/Header/index.js
@@ -5,12 +5,14 @@ import image from '../../../../assets/avatar.png';
 import './styles.scss';
 import Logo from './Logo';
 import { useNavigate } from 'react-router-dom';
+import SearchBar from './Searchbar';
 
 export default function Header() {
   const navigate = useNavigate();
   return (
     <header className="container">
       <Logo />
+      <SearchBar />
       <img src={image} alt={'Avatar'} onClick={() => navigate('/profile')} />
     </header>
   );
