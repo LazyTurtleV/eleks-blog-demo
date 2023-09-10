1ed95c2f0743d8b4ebc2fe8380ed3af3ea17388a
LazyTurtleV <savinvolodya001@gmail.com>
2023-09-10 14:38:35 +0300

    take header to separate file

--- src/Pages/Story/index.js
+++ src/Pages/Story/index.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-
-import likeBtn from '../../../assets/like-button.svg';
-import checkedLikeBtn from '../../../assets/like-button-checked.svg';
-import arrowLeft from './../../../assets/arrow-left.svg';
+import { useParams } from 'react-router-dom';
 
 import AppHeader from '../Common/Header';
 import DataLayer from '../../Services/DataLayer';
 
 import styles from './styles.module.scss';
 import useBreakpoints from '../Common/useBreakpoints';
+import Header, { LikeButton } from './Header';
 
 export default function Story() {
   const [story, setStory] = useState();
@@ -43,52 +40,5 @@ function Article({ story }) {
         />
       </header>
     </main>
-  );
-}
-
-function Header({ likesNumber }) {
-  const navigate = useNavigate();
-  const { breakpoint } = useBreakpoints();
-  return (
-    <header className={styles.storyHeader}>
-      <button
-        className={styles.backButton}
-        onClick={() => navigate('/stories')}
-      >
-        <img className={styles.backButton.img} src={arrowLeft} alt={'Back'} />
-        <h1 className={styles.backButton.h1}>All stories</h1>
-      </button>
-      {['small', 'medium'].includes(breakpoint) && (
-        <LikeButton likesNumber={likesNumber} mobile />
-      )}
-    </header>
-  );
-}
-
-function LikeButton({ likesNumber = 0, mobile }) {
-  const [isChecked, check] = useState(false);
-  const [likes, setLikes] = useState(0);
-
-  useEffect(() => {
-    setLikes(likesNumber);
-  }, [likesNumber]);
-
-  const onClick = () => {
-    if (isChecked) {
-      setLikes((p) => p - 1);
-    } else {
-      setLikes((p) => p + 1);
-    }
-    check((p) => !p);
-  };
-
-  return (
-    <aside
-      className={styles.likeBtn}
-      style={!mobile ? { flexDirection: 'column', gap: '10px' } : undefined}
-    >
-      <img src={isChecked ? checkedLikeBtn : likeBtn} onClick={onClick} />
-      <p>{likes} likes</p>
-    </aside>
   );
 }
