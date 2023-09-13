94fa6e1b577bade7bc7b7fd4da5a8c2ceadebb5a
LazyTurtleV <savinvolodya001@gmail.com>
2023-09-13 23:32:47 +0300

    initial addition page styling

--- src/Pages/Add/index.js
+++ src/Pages/Add/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppHeader from '../Common/Header';
 import Header from './Header';
 
 import styles from './styles.module.scss';
+import useResizableTextarea from '../Common/useResizableTextarea';
 
 export default function Add() {
+  const [text, setText] = useState('');
+  const [title, setTitle] = useState('');
+  const ref = useResizableTextarea(text);
+
   return (
     <div className={styles.container}>
       <AppHeader hideSearchBar />
       <Header />
+      <main className={styles.article}>
+        <input
+          className={styles.title}
+          placeholder="Title"
+          onChange={getSetter(setTitle)}
+        />
+        <textarea
+          ref={ref}
+          className={styles.text}
+          placeholder="Type text"
+          onChange={getSetter(setText)}
+        />
+      </main>
     </div>
   );
+}
+
+function getSetter(setter) {
+  return (e) => setter(e.target.value);
 }
