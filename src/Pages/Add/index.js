205a5b7acbda11c9ff48a8b06f3d5d80464d1aca
LazyTurtleV <savinvolodya001@gmail.com>
2023-09-13 20:19:18 +0300

    add addition page

---
+++ src/Pages/Add/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Header from '../Common/Header';
+
+export default function Add() {
+  return <Header />;
+}
--- src/Pages/Stories/StoriesGridHeader.js
+++ src/Pages/Stories/StoriesGridHeader.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function StoriesGridHeader() {
+  const navigate = useNavigate();
   return (
     <header className={styles['stories-grid-header']}>
       <h1 className={styles.h1}>Top stories</h1>
-      <button className={styles['add-story-btn']}>Add story</button>
+      <button
+        className={styles['add-story-btn']}
+        onClick={() => navigate('../add')}
+      >
+        Add story
+      </button>
     </header>
   );
 }
