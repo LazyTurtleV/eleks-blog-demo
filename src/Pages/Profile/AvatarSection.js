8ae55a479e0587f162e69d40b0e15dc446acfcf6
LazyTurtleV <savinvolodya001@gmail.com>
2023-09-17 20:22:42 +0300

    elaborate profile form

--- src/Pages/Profile/AvatarSection.js
+++ src/Pages/Profile/AvatarSection.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import avatar from '../../../assets/avatar.png';
+import attachment from '../../../assets/attachment.svg';
 
 import styles from './styles.module.scss';
 
 export default function AvatarSection() {
+  const [file, setFile] = useState(null);
+  const ref = useRef(null);
   return (
     <section className={styles.avatarSection}>
-      <img src={avatar} />
+      <input
+        type="file"
+        ref={ref}
+        onChange={(e) => setFile(URL.createObjectURL(e.target.files[0]))}
+      />
+      <img src={file || avatar} />
+      <button className={styles.editButton} onClick={() => ref.current.click()}>
+        <img src={attachment} />
+        <p>Edit avatar</p>
+      </button>
     </section>
   );
 }
