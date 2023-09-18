86f96d80b5a6b061f492b165879d7e6c4b3ad69b
LazyTurtleV <savinvolodya001@gmail.com>
2023-09-18 20:40:39 +0300

    use cookie to store auth

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -1,8 +1,17 @@
-import React, { createContext, useCallback, useContext, useState } from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import Cookie from 'universal-cookie';
 
 export const Context = createContext({});
 
 export const useAuthContext = () => useContext(Context);
+
+const cookie = new Cookie();
 
 async function requestToAuthIdentityManager({ login, password }) {
   if (login === 'quadro@gmail.com' && password === '1') {
@@ -23,15 +32,23 @@ export function AuthContext({ children }) {
   });
   const [onErrorCallbacks, setOnErrorCallbacks] = useState([]);
 
+  useEffect(() => {
+    const auth = cookie.get('auth');
+    if (auth) {
+      setAuthInfo({ isAuthorized: true, access_token: auth });
+    }
+  }, [setAuthInfo]);
+
   const login = useCallback(
     (loginVar, password) => {
       requestToAuthIdentityManager({ login: loginVar, password })
-        .then(({ access_token }) =>
+        .then(({ access_token }) => {
           setAuthInfo({
             isAuthorized: true,
             access_token,
-          })
-        )
+          });
+          cookie.set('auth', access_token);
+        })
         .catch((err) => {
           for (const callback of onErrorCallbacks) {
             callback(err);
