87a1b11e2676475d145d1a994b585a927e7a3e5d
LazyTurtleV <savinvolodya001@gmail.com>
2023-10-13 22:47:34 +0300

    fix ts error

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
       {
         path: '/',
         loader() {
-          throw new redirect('stories');
+          return redirect('stories');
         },
       },
       {
