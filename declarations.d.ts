37eb1df4588bd0f4fd545726e9237aab39d32a9e
LazyTurtleV <savinvolodya001@gmail.com>
2023-10-14 17:01:18 +0300

    add support for images urls

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,5 +1,9 @@
 declare module '*.scss'; 
 
+declare module "*.png" {
+    const value: any;
+    export default value;
+}
 declare module "*.svg" {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
     export default content;
